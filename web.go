07ee41d8bff830b861425e05de2361a442300d1a
Justin Mearns <justin.mearns@gmail.com>
2023-05-07 21:16:22 -0600

    Created a simple REST app

--- web.go
+++ web.go
@@ -1,14 +1,62 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(writer, "hello, you've requested %s\n", r.URL.Path)
-	})
+type album struct {
+	ID     string
+	Title  string
+	Artist string
+	Price  float64
+}
 
-	http.ListenAndServe((":80"), nil)
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+
+	// routes
+
+	// GET
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+
+	// POST
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
+func getAlbums(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(context *gin.Context) {
+	var newAlbum album
+
+	if err := context.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	context.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(context *gin.Context) {
+	id := context.Param("id")
+
+	for _, album := range albums {
+		if album.ID == id {
+			context.IndentedJSON(http.StatusOK, album)
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
